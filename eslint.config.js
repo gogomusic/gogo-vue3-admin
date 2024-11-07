e8606fbf50169407d699db56f60672fac577b9cc
dengtz <dengtz@innosilicon.com.cn>
2024-11-07 19:42:59 +0800

    fix: 修复ESLint插件BUG（配置错误）

--- eslint.config.js
+++ eslint.config.js
@@ -5,29 +5,21 @@ import pluginVue from 'eslint-plugin-vue';
 export default [
 	{
 		name: 'app/files-to-lint',
-		files: ['**/*.{.js,ts,mts,tsx,vue}'],
+		files: ['**/*.{ts,tsx,vue}'],
 	},
 	{
 		name: 'app/files-to-ignore',
-		ignores: [
-			'**/dist/**',
-			'**/dist-ssr/**',
-			'**/coverage/**',
-			'*.cjs',
-			'*.config.ts',
-		],
+		ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
 	},
 	...pluginVue.configs['flat/essential'],
 	...vueTsEslintConfig(),
 	skipFormatting,
 	{
 		rules: {
-			'@typescript-eslint/no-explicit-any': 'off',
 			'vue/multi-word-component-names': 'off',
+			'@typescript-eslint/no-explicit-any': 'off',
 			'vue/no-unused-components': 'error',
 			'vue/no-undef-components': 'error',
-			'no-unused-vars': 'off',
-			'no-undef': 'off',
 		},
 	},
 ];
